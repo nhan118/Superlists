90c4c1965a5b60aa4989958ec9c1a892f1b76b3e
Ryan Han <ryanhan@objectivasoftware.com>
2018-07-05 19:00:07 +0800

    modify FT file

--- TestScripts/fuctional_tests.py
+++ TestScripts/fuctional_tests.py
@@ -1,14 +1,37 @@
 # -*- coding:utf-8 -*-
 
 from selenium import webdriver
-import time
+import unittest
 
 
-browser = webdriver.Chrome()
-browser.get("http://localhost:8000")
-browser.maximize_window()
-time.sleep(3)
-assert 'Django' in browser.title
-browser.quit()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def can_start_a_list_and_retirvev_it_later(self):
+        # Ryan hear about a new cool online to-do app.
+        # He goes to check out its homepage.
+        self.browser.get("http://localhost:8000")
+
+        # He notices the page tile and header mention to-do list.
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+        # He is invited to enter a to-do item straight away
+
+        # He types "Buy peacock feathers"(Ryan's hobby is tying fly-fishing lures)
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
 
 
