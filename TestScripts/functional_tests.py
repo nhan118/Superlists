af3cb96dde673537f465fd7ffaebd65fa8d486e5
HanNing <themail@sohu.com>
2018-07-15 21:55:56 +0800

    create a function named "check_for_row_in_list_table"

--- TestScripts/functional_tests.py
+++ TestScripts/functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Ryan hear about a new cool online to-do app.
         # He goes to check out its homepage.
@@ -41,9 +46,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        # table = self.browser.find_element_by_id("id_list_table")
-        # rows = table.find_elements_by_tag_name("tr")
-        # self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
         # There is still a text box inviting him to add another item. He
         # enters "Use peacock feathers to make a fly" (Ryan is very methodical)
         inputbox = self.browser.find_element_by_id("id_new_item")
@@ -53,8 +58,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # The page updates again, and now shows both items on her list
 
