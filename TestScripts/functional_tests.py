bfd1c73c4daad350e8e856b4a506f2c62b6d39a3
HanNing <themail@sohu.com>
2018-07-15 21:47:20 +0800

    section 5.4

--- TestScripts/functional_tests.py
+++ TestScripts/functional_tests.py
@@ -41,22 +41,27 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows),
-            "New to-do item did not appear in table"
-        )
+        # table = self.browser.find_element_by_id("id_list_table")
+        # rows = table.find_elements_by_tag_name("tr")
+        # self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
         # There is still a text box inviting him to add another item. He
         # enters "Use peacock feathers to make a fly" (Ryan is very methodical)
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
 
         # The page updates again, and now shows both items on her list
 
         # Ryan wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him -- there is some
         # explanatory text to that effect.
-
+        self.fail("Finish the test!")
         # He visits that URL - his to-do list is still there.
 
         # Satisfied, he goes back to sleep.
